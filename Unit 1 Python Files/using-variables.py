bc22786a20003434c68ebfd1f49dc78b71a9f352
Ryan James <ryanjames1729@gmail.com>
2021-08-23 09:47:28 -0400

    Add files via upload

---
+++ Unit 1 Python Files/using-variables.py
@@ -0,0 +1,16 @@
+# CDS - Programming
+# Using Variables Assignment
+#
+# Student Name: TEACHER SOLUTION
+
+# Creating variables of each type
+a = -1      # int type (integer)
+
+b = 2.7     # float type (decimal)
+
+c = False   # bool type (Boolean)
+
+d = "CDS"   # str type (string)
+
+
+# My variable var
