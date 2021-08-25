1f65a0c2ba251318ad8ad36b558c08f62c786428
Ryan James <ryanjames1729@gmail.com>
2021-08-25 14:28:49 -0400

    Add files via upload

---
+++ Unit 1 Python Files/digit-sum.py
@@ -0,0 +1,14 @@
+# CDS - Programming
+# Digit Sum Assignment
+#
+# Student Name: 
+
+# Ask for user input
+
+
+# Run the calculations
+
+
+
+
+# Print the outcome of the calculations
---
+++ Unit 1 Python Files/is-odd.py
@@ -0,0 +1,14 @@
+# CDS - Programming
+# Is Odd Assignment
+#
+# Student Name: 
+
+# Ask for input
+
+
+# Determine if the number is even or odd
+odd = False
+
+
+
+# Print a statement with the result
