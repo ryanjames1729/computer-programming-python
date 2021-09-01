fcd5a4b59e7c9fa7cdb23ee9f123a19cba1d4a59
Ryan James <ryanjames1729@gmail.com>
2021-09-01 15:04:47 -0400

    Add files via upload

---
+++ Unit 1 Python Files/calculator-1.py
@@ -0,0 +1,20 @@
+# CDS - Programming
+# Calculator-1 Assignment
+#
+# Student Name: 
+
+# Ask for input (two inputs)
+
+
+
+# Create variables for each of the 6 operations
+# ***the output for remainder division should be a String***
+
+
+
+
+
+
+
+# Print the output of the operations by printing the original input and the new variables
+
