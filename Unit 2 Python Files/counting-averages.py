53cc72fd20f4a42d93683a1e7d2d8ea36752b22d
Ryan James <ryanjames1729@gmail.com>
2021-09-10 11:18:35 -0400

    Add files via upload

---
+++ Unit 2 Python Files/counting-averages.py
@@ -0,0 +1,17 @@
+# CDS - Programming
+# Counting Averages Assignment
+#
+# Student Name: TEACHER SOLUTION
+
+# Create variables needed for program
+entryCount = 0
+entry = -1
+total = 0
+
+# Create a loop to get input
+
+
+# If a number is typed, add to the total
+
+
+# Print the output
