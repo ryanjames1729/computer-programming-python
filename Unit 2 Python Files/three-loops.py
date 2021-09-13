1a5801a1d98d51665b56d1c077144f61ed89d2fc
Ryan James <ryanjames1729@gmail.com>
2021-09-13 09:49:23 -0400

    Add files via upload

---
+++ Unit 2 Python Files/three-loops.py
@@ -0,0 +1,16 @@
+# CDS - Programming
+# Three Loops Assignment
+#
+# Student Name: 
+
+# Get input from the user as a String (use 'var' as your variable)
+
+
+
+length = len(var)
+# Create a 'while' loop to print the characters of the message.
+
+
+# Create a 'for' loop to print the characters of the message.
+
+# Create a 'for each' loop print the characters of the message.
