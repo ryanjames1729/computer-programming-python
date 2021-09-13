4cbc8f4f79ad312a9eb7d7d2f267c3d7045e1754
Ryan James <ryanjames1729@gmail.com>
2021-09-13 10:06:46 -0400

    Add files via upload

---
+++ Unit 2 Python Files/fizz-buzz.py
@@ -0,0 +1,10 @@
+# CDS - Programming
+# Fizz Buzz Assignment
+#
+# Student Name: 
+
+# Print integers 1 to N, but print “Fizz” if an integer is divisible by 3, 
+# “Buzz” if an integer is divisible by 5, and “FizzBuzz” if an integer is 
+# divisible by both 3 and 5.
+
+N = int(input("Enter an integer greater than 1:>>"))
