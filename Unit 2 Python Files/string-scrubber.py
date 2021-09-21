8ea3d96cc3b5f6e779556f377e8fd978b7c79be8
Ryan James <ryanjames1729@gmail.com>
2021-09-21 09:19:22 -0400

    Add files via upload

---
+++ Unit 2 Python Files/string-scrubber.py
@@ -0,0 +1,21 @@
+# CDS - Programming
+# String Scrubber Assignment
+#
+# Student Name: TEACHER SOLUTION
+
+# Starting string - don't edit
+startingString = "xxXThis ixxXs the message. Make sure that you crxxXeate a waXxXy to decode the messXxXage. It has been encoxXxded for tXxXhe safety of everyone tryinxXxg to read this messagexXx."
+
+# Shift everything to lower case
+# Using startingString.lower() creates a new string with all letters lowercase
+
+
+# Remove all strings with xxx using a loop
+
+
+# Go back and capitalize letters at indices 0, 21, 76
+
+
+
+# Print the output
+print(startingString)
