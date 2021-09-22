f7b2225e6b862aa1bece3edd28a3a37199ccadaf
Ryan James <ryanjames1729@gmail.com>
2021-09-22 12:23:10 -0400

    Add files via upload

---
+++ Unit 2 Python Files/caesar-cipher.py
@@ -0,0 +1,21 @@
+# CDS - Programming
+# Caesar Cipher Assignment
+#
+# Student Name: TEACHER SOLUTION
+
+testWord = "cat"
+testShift = 19
+
+originalWord = input("Give me a word to encrypt:>>")
+shift = int(input("Type the key as an integer number:>>"))
+newWord = ""                
+
+# originalWord = testWord
+# shift = testShift
+
+# Create a loop that will run the length of the word with the algorithm inside the loop.
+
+
+# Testcase output: "vtm"
+
+# Print the output with the original word and the new word
