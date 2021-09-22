cbeee15283adbdf62d4e46a0fe54ad46f8e01d93
Ryan James <ryanjames1729@gmail.com>
2021-09-22 10:19:56 -0400

    Add files via upload

---
+++ Unit 2 Python Files/pig-latin.py
@@ -0,0 +1,25 @@
+# CDS - Programming
+# Pig Latin Assignment
+#
+# Student Name: TEACHER SOLUTION
+
+# Strings for Testing
+test1 = "pig"       # this should yield 'igpay'
+test2 = "smile"     # this should yield 'ilesmay'
+test3 = "eat"       # this should yield 'eatyay'
+
+# Get the input as a word
+word = input("Give a word to translate:>>")
+newWord = ""        # this will be the new translated word
+
+
+
+# Get the first two letters and assign them to variables
+
+
+# Write an algorithm to translate correctly
+
+
+
+# Print the output
+print("{} becomes {} in pig latin.".format(word, newWord))
