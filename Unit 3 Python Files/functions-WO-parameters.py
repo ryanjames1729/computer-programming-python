287d8beff983e679c8de02eee8eef02e763ddcd4
Ryan James <ryanjames1729@gmail.com>
2021-10-18 11:34:42 -0400

    Add files via upload

---
+++ Unit 3 Python Files/functions-WO-parameters.py
@@ -0,0 +1,38 @@
+# CDS - Programming
+# Functions WO Parameters Assignment
+#
+# Student Name: 
+
+import random
+
+# Create a function called 'greeting()'
+# This function should not have any input parameters.
+# This function should just print the msg 'Hey there, have we met?'
+
+
+
+
+
+# Create a function called 'greeting2()'
+# This function should not have any input parameters.
+# This function should return (not print) the msg 'Hey there, do we know each other?'
+
+
+
+
+# Call the functions 'greeting' and 'greeting2' 
+# Make sure to call them correctly so both messages are printed
+
+
+
+
+# Create a function called 'primes()'
+# This function should not have any input parameters.
+# This function should generate a prime number using the following algorithm:
+# 1. generate a random number using: num = random.randint(2,1000)
+# 2. create a variable: prime = True
+# 3. using a loop, determine if this is a prime number
+# 4. at the end of the loop either return the prime number or call the primes() function again
+
+
+# Call the prime number generator function and print it correctly
