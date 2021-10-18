1c7eb585c49b78200e6ce494742a7e50e93a14ec
Ryan James <ryanjames1729@gmail.com>
2021-10-18 11:38:25 -0400

    Add files via upload

---
+++ Unit 3 Python Files/functions-with-parameters.py
@@ -0,0 +1,24 @@
+# CDS - Programming
+# Functions with Parameters Assignment
+#
+# Student Name:
+
+# Write a function called 'sum'.
+# This function should have 2 input parameters, a and b.
+# This function should return the sum of a and b.
+
+
+
+# Write a function called 'max'.
+# This function should have 2 input parameters, a and b.
+# This function shoud return which value is greater: a or b.
+
+
+
+# Write a function called 'power'.
+# This function should have 2 input parameters, base and exponent
+# This function should return the value for base^exponent. You will need to use a loop to achieve this.
+
+
+
+# Call all 3 functions using the correct function calls and print statements.
